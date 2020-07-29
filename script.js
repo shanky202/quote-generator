4440bbd9c35bc12ca00951c56162d1d7c8838211
shanky202 <shankypachore@gmail.com>
2020-07-29 12:35:04 +0000

    added project files

---
+++ script.js
@@ -0,0 +1,88 @@
+const quoteContaier = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const whatsapBtn = document.getElementById("whatsapp");
+const facebookBtn = document.getElementById("facebook");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader")
+
+
+
+// Get Quote From API
+async function getQuote() {
+    ShowLoadingSpinner();
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+    const apiUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add "Unknown"
+        if (data.quoteAuthor === "") {
+            authorText.innerText = "Unknown"
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        };
+        // Reduce font-size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote')
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        quoteText.innerText = data.quoteText;
+        ShowLoadingSpinner();
+    } catch (err) {
+        getQuote();
+
+    }
+}
+
+function ShowLoadingSpinner() {
+    loader.hidden = false;
+    quoteContaier.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContaier.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+// Tweet Quote
+function teewtQuote() {
+
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(tweetUrl, "_blank")
+}
+
+// Share on Whatsapp
+function whatsappShare() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const whatsappUrl = `whatsapp://send?text=${quote} - ${author}`
+    window.open(whatsappUrl)
+}
+
+// Share on Facebook
+function facebookShare() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const facebookUrl = `http://www.facebook.com/sharer.php?s=${quote}`;
+    window.open(facebookUrl)
+}
+
+
+
+// Event Listener
+
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', teewtQuote);
+whatsapBtn.addEventListener('click', whatsappShare);
+facebookBtn.addEventListener('click', facebookShare);
+
+//  On Load
+getQuote();
+loading();
